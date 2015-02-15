0ea08ef43cde83dfbf13d832b2b183385daac04e
webulyanov <web.ulyanov@gmail.com>
2015-02-15 23:13:06 +0400

    update style code

--- js/jquery.parsingGet.js
+++ js/jquery.parsingGet.js
@@ -20,22 +20,22 @@
 
 		// С какими элементами будем работать
 		var arrInput = [
-										'input[type="text"]', 
-										'input[type="color"]',
-										'input[type="date"]',
-										'input[type="hidden"]',
-										'input[type="datetime"]',
-										'input[type="datetime-local"]',
-										'input[type="email"]',
-										'input[type="number"]',
-										'input[type="range"]',
-										'input[type="search"]',
-										'input[type="tel"]',
-										'input[type="time"]',
-										'input[type="url"]',
-										'input[type="month"]',
-										'input[type="week"]'
-									];
+			'input[type="text"]', 
+			'input[type="color"]',
+			'input[type="date"]',
+			'input[type="hidden"]',
+			'input[type="datetime"]',
+			'input[type="datetime-local"]',
+			'input[type="email"]',
+			'input[type="number"]',
+			'input[type="range"]',
+			'input[type="search"]',
+			'input[type="tel"]',
+			'input[type="time"]',
+			'input[type="url"]',
+			'input[type="month"]',
+			'input[type="week"]'
+		];
 
 		// Получение и разбор GET параметров в ассоциативный массив
 		var url = window.location.href;
