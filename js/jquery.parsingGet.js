2873a7c78c0597ec20b77cb6a6b6e667574c2e25
webulyanov <web.ulyanov@yandex.ru>
2015-02-15 19:40:40 +0400

    Template Plugin

---
+++ js/jquery.parsingGet.js
@@ -0,0 +1,28 @@
+/*
+
+ ParsingGet — jQuery plugin
+ Version: 0.0.1
+ Author: M.Ulyanov (web.ulyanov@gmail.com)
+ Site: http://web-ulyanov.ru
+ Source && Doc: https://github.com/M-Ulyanov/ParsingGet
+ Example: #
+
+ */
+
+(function($) {
+	jQuery.fn.lprogress = function(options){
+
+		// Параметры по умолчанию
+		var defaults = $.extend({}, {
+		
+		}, options);
+				
+		return this.each(function() {
+				
+		});
+	}
+})(jQuery);
+
+
+
+
